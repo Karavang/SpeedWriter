08bfd28492a5fb023a87b5ff12a348e0243d91a2
Karavang <k4r4v4ng@gmail.com>
2024-06-11 15:28:38 +0200

    Complete

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,39 +1,78 @@
 ﻿#include <iostream>
 #include <string>
-#include <cstring>
-#include <windows.h>
 #include <vector>
+#include <conio.h>
+#include <windows.h>
+#include <chrono>
+#include <iomanip> // Для форматирования вывода
+
 using namespace std;
+using namespace std::chrono;
 
-
+// Устанавливает цвет текста в консоли
 void SetColor(int color) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+// Функция для вывода текста с раскрашиванием введенных символов
 void PrintWithColor(const string& text, int matchedLength) {
-	for (size_t i = 0; i < text.length(); ++i) {
-		if (i < matchedLength) {
-			SetColor(10); // Салатовый цвет (светло-зеленый)
-		}
-		else {
-			SetColor(15); // Белый цвет
-		}
-		cout << text[i];
-	}
-	SetColor(15); 
+    system("CLS"); // Очищает экран консоли
+    for (size_t i = 0; i < text.length(); ++i) {
+        if (i < matchedLength) {
+            SetColor(10); // Салатовый цвет (светло-зеленый)
+        }
+        else {
+            SetColor(15); // Белый цвет
+        }
+        cout << text[i];
+    }
+    SetColor(15); // Восстановить белый цвет
+    cout << endl;
 }
 
 int main() {
-	vector<char> strrr;
-	cout << "Enter text"<<endl;
-	string input;
-	getline(cin, input);	
-	for (char ch : input) {
-		strrr.push_back(ch);
-	}
-	strrr.push_back('\0');
-	SetColor(10);
-	system("CLS");
-	cout <<  strrr.data()  << endl;
-	system("pause");
+    string input;
+
+    // Цикл для запроса текста, пока не будет введено что-либо
+    do {
+        cout << "Enter text: ";
+        getline(cin, input);
+
+        if (input.empty()) {
+            cout << "No text entered. Please enter some text." << endl;
+        }
+    } while (input.empty());
+
+    vector<char> strrr(input.begin(), input.end());
+    strrr.push_back('\0');
+
+    int matchedLength = 0;
+    auto start = high_resolution_clock::time_point();
+    bool started = false;
+
+    // Изначально выводим пустую строку с исходным текстом
+    PrintWithColor(input, matchedLength);
+
+    while (matchedLength < input.length()) {
+        if (_kbhit()) { // Проверяет, была ли нажата клавиша
+            char ch = _getch(); // Считывает символ с клавиатуры
+            if (!started) {
+                start = high_resolution_clock::now(); // Захватывает время начала ввода первого символа
+                started = true;
+            }
+            if (ch == input[matchedLength]) {
+                matchedLength++; // Увеличивает счетчик совпавших символов
+            }
+            PrintWithColor(input, matchedLength); 
+        }
+    }
+
+    auto end = high_resolution_clock::now(); // Захватывает время окончания ввода последнего символа
+    duration<double> diff = end - start; // Рассчитывает продолжительность ввода в секундах
+
+    cout << "Completed" << endl;
+    cout << fixed << setprecision(2) << "Time taken: " << diff.count() << "s." << endl; // Вывод времени с двумя знаками после запятой
+
+    system("pause");
+    return 0;
 }
